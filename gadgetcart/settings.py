c5586fca9f8929d60e64e122183a6f876d8a5ec8
Vishnu <vpvishnu2000@gmail.com>
2021-12-13 12:55:17 +0530

    AH update

--- gadgetcart/settings.py
+++ gadgetcart/settings.py
@@ -30,7 +30,7 @@ VERIFICATION_KEY = config('verification_sid')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool, default=True)
 
-ALLOWED_HOSTS = [ '127.0.0.1','68.183.91.11', '139.59.32.61' , '165.232.189.72', 'www.gadgetcart.ga', 'gadgetcart.ga' ]
+ALLOWED_HOSTS = [ '127.0.0.1', '139.59.32.61' , '165.232.189.72', 'www.gadgetcart.ga', 'gadgetcart.ga' ]
 
 
 # Application definition
